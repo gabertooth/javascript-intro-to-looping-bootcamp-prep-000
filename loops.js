45fc6f5e4029b9d803b0c06ffb64285689967f25
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-16 15:27:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function incrementVariable(){
   do {
     array.pop;
     }
-    while (array.length > 0 && incrementVariable())
+    while (array.length > 0 && incrementVariable()){
 }
 return array
 }
