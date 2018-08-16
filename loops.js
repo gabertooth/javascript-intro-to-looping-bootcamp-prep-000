08f5975f86d82fcf68c96961bb43f12084f3b06a
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-16 14:45:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,7 +15,10 @@ function whileLoop(n){
   }
    return 'done'
 }
-var incrementVariable()=
+var i = 0 
+function incrementVariable(){
+  i=i+1;
+}
 function doWhileLoop(array){
   do {
     [array.shift];
